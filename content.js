40508c72dc0e7330ff73ab1ed6a0a5823c6d7283
Ashutosh Ranjan <ashutoshranjan@kgpian.iitkgp.ac.in>
2026-06-22 18:46:26 +0530

    Add page on webpage

--- content.js
+++ content.js
@@ -1,7 +1,17 @@
 // content.js
-// Author:
-// Author URI: https://
-// Author Github URI: https://www.github.com/
-// Project Repository URI: https://github.com/
 // Description: Handles all the webpage level activities (e.g. manipulating page data, etc.)
-// License: MIT
+const bookmarkIMG = chrome.runtime.getURL("assets/bookmark.png");
+
+window.addEventListener("load",addBookmarkButton);
+
+function addBookmarkButton(){
+    const bookmarkbutton=document.createElement('img');
+    bookmarkbutton.id="add-bookmark-button";
+    bookmarkbutton.src = bookmarkIMG;
+    bookmarkbutton.style.height="30px";
+    bookmarkbutton.style.width="30px";
+
+    const description=document.getElementsByClassName("font-open-sans text-[16px] leading-[130%] font-bold text-fg-grey-primary")[0];
+
+    description.insertAdjacentElement("beforebegin",bookmarkbutton);
+}
